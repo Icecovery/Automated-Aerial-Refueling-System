29a9793caa57dbdc177b6e56279c225ea76f633d
Billy Peng <858700259@qq.com>
2017-10-11 19:55:35 +0800

    Add AARS Category

---
+++ AARS/AARS/Properties/Category.cs
@@ -0,0 +1,30 @@
+﻿using KSP.UI.Screens;
+using System.Collections.Generic;
+using UnityEngine;
+
+    [KSPAddon(KSPAddon.Startup.MainMenu, true)]
+    public class Category : MonoBehaviour
+    {
+        private static readonly List<AvailablePart> availableParts = new List<AvailablePart>();
+
+        void Awake()
+        {
+            GameEvents.onGUIEditorToolbarReady.Add(AARSCategoryFunc);
+        }
+
+        void AARSCategoryFunc()
+        {
+            const string customCategoryName = "AARS";
+            const string customDisplayCategoryName = "AARS Parts";
+            availableParts.Clear();
+            availableParts.AddRange(PartLoader.LoadedPartsList.AARSParts());
+            Texture2D iconTex = GameDatabase.Instance.GetTexture("AARS/Textures/AARSIcon", false);
+            RUI.Icons.Selectable.Icon icon = new RUI.Icons.Selectable.Icon("AARS", iconTex, iconTex, false);
+            PartCategorizer.Category filter = PartCategorizer.Instance.filters.Find(f => f.button.categoryName == "Filter by Function");
+
+            if (filter == null)
+                Debug.Log("filter is null");
+            else
+                PartCategorizer.AddCustomSubcategoryFilter(filter, customCategoryName, customDisplayCategoryName, icon, p => availableParts.Contains(p));
+        }
+    }
---
+++ AARS/AARS/Properties/PartLoaderExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+    public static class PartLoaderExtensions
+    {
+        public static IEnumerable<AvailablePart> AARSParts(this List<AvailablePart> parts)
+        {
+            return (from avPart in parts.Where(p => p.partPrefab)
+                    let AARSModule = avPart.partPrefab.GetComponent<IsAARSpart>()
+                    where AARSModule != null
+                    select avPart
+                     ).ToList();
+        }
+    }
